e3e5352cf1427d8d4a10ad0189caadcd4854268c
Alimardon9459 <mullajonovalimardon@gmail.com>
2024-01-29 23:36:47 +0500

    frist commit

---
+++ front/just/src/components/forAddProduct/InputGroup.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+import { useCentralStore } from '../../stores/centralStore'
+const store = useCentralStore()
+const name = ref()
+const bar_code = ref()
+
+</script>
+
+<template>
+    <div>
+        {{ store.api }}
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
--- front/just/src/routes.js
+++ front/just/src/routes.js
@@ -2,11 +2,13 @@
 import IndexPage from './pages/IndexPage.vue'
 import ReportPage from './pages/ReportPage.vue'
 import AuthPage from './pages/AuthPage.vue'
+import AddProductPage from './pages/AddProduct.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   { path: '/', component: IndexPage },
   { path: '/report', component: ReportPage },
   { path: '/auth', component: AuthPage },
+  { path: '/addproduct', component: AddProductPage },
 ]
 // 3. Create the router instance and pass the `routes` configuration
 // You can pass in additional options here, but let's keep it simple for now.
