708d2cbad5c4e441777af0991a7dce8e2f30f94c
Alimardon <100981157+Alimardon9459@users.noreply.github.com>
2024-01-29 23:40:00 +0500

    frist commit (#3)

---
+++ front/just/src/pages/AddProduct.vue
@@ -0,0 +1,15 @@
+<script setup>
+import InputGroup from '../components/forAddProduct/InputGroup.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <InputGroup/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
