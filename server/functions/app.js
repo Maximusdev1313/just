a5e9879452a861546b7fd761bb3f3c796e7f0da5
Maximusdev1313 <maximusdev1313@gmail.com>
2024-07-28 23:05:27 +0500

    change: cors

--- server/functions/app.js
+++ server/functions/app.js
@@ -11,7 +11,7 @@ const router = express.Router();
 const port = process.env.PORT || 5000
 process.env.TZ = "Asia/Tashkent"
 
-app.use(cors({ origin: ['https://razzoq.uz', 'http://localhost:5173', 'http://localhost:5174', 'https://razzoq.netlify.app/', 'https://justuz.netlify.app',] }))
+app.use(cors({ origin: ['https://razzoq.uz', 'http://localhost:5173', 'http://localhost:5174', 'https://razzoq.netlify.app', 'https://justuz.netlify.app',] }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
