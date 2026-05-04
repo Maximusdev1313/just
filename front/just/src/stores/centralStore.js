9906f00dadbe3182cb4e4902f0afd3bfbfaa35ad
maximusdev1313 <maximusdev1313@gmail.com>
2026-05-05 01:31:21 +0500

    change: api

--- front/just/src/stores/centralStore.js
+++ front/just/src/stores/centralStore.js
@@ -6,7 +6,7 @@ import { format } from "date-fns";
 
 export const useCentralStore = defineStore('central', {
   state: () => ({
-    api: 'http://localhost:8888/.netlify/functions/app',
+    api: 'https://justserv.netlify.app/.netlify/functions/app',
     items: [],
     itemsForSell: [],
     cartOpen: false,
